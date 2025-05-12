f6ac927f1244e8a5bb5d99ae96514b721118f9d8
Dmitry Ustalov <dmitry.ustalov@jetbrains.com>
2025-05-12 16:18:44 +0200

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,8 +1,10 @@
+#!/bin/bash -x
+
 STAGE=$1
 LANGUAGE=$2
 
 unzip data/$LANGUAGE-$STAGE -d data/repositories-$LANGUAGE-$STAGE
 
 for zipfile in data/repositories-$LANGUAGE-$STAGE/*.zip; do
-  unzip "$zipfile" -d "${zipfile%.zip}"
+  unzip -o "$zipfile" -d "${zipfile%.zip}"
 done
